b66bde370c916a5425cd21e58addb4b0c14e2476
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-08-20 16:05:26 -0500

    Implements very basic wireframe for application

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const start = () => true
+  const stop = () => false
+  const isRunning = () => false
+  const clearGrid = () => null
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Conway's Game of Life</h1>
       </header>
+      <body className="App-body">
+        <div className="grid">
+          <h2>grid</h2>
+        </div>
+        <button onClick={() => { 
+          if(isRunning()){
+            stop() ; 
+          }
+          else{
+            start() ; 
+          }
+        }}>{isRunning()? 'Stop' : 'Start'}</button>
+        <button disabled={isRunning()} onClick={() => {
+          if(!isRunning()){
+            clearGrid()
+          }
+        }}>{isRunning() ? '...' : "Clear"}</button>
+      </body>
     </div>
   );
 }
