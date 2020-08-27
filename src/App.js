ab7e1072dfe5de4a130417b8dc477a1068821ca8
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-08-27 15:37:48 -0500

    Styles for smaller screens, better UX

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import useConways from './hooks/useConways';
 
 function App() {
 
-  const [rwClm, setRwClm] = useState(50);
+  const [rwClm, setRwClm] = useState(39);
 
   const { 
     random,
@@ -25,12 +25,43 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Conway's Game of Life</h1>
-      </header>
       <div className="App-body">
         <div className="controls-container">
-          <label htmlFor="play">Play the Game!</label>
+          <label htmlFor="about">About Game of Life:</label>
+          <p name="about" className="about">
+            <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">The Game of Life</a> is a cellular automaton devised by 
+            John Horton Conway. It is a zero-player game, meaning that its evolution 
+            is determined by its initial state.
+          </p>
+          <label htmlFor="rules">Rules:</label>
+          <ol name="rules" className="rules">
+            <li>Any live cell with two or three live neighbours survives.</li>
+            <li>Any dead cell with three live neighbours becomes a live cell.</li>
+            <li>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</li>
+          </ol>
+          <hr />
+          <label htmlFor="meth">Seed</label>
+          <div name='meth' className="methuselah-container button-container">
+            <button onClick={() => {
+              clear()
+              setGrid(random())
+            }}>Random</button>
+            <button onClick={() => {
+              clear()
+              setGrid(acorn())
+            }}>Acorn</button>
+            <button onClick={() => {
+              clear()
+              setGrid(rPentomino())
+            }}>rPentomino</button>
+            <button onClick={() => {
+              clear()
+              setGrid(thunderbird())
+            }}>Thunderbird</button>
+          </div>
+          <small>(or select nodes in the grid to initialize manually)</small>
+          <hr />
+          <label htmlFor="play">Play!</label>
           <div name='play' className="main-controls button-container">
             <button onClick={() => { 
               if(isEvolving()){
@@ -46,51 +77,8 @@ function App() {
               }
             }}>{isEvolving() ? 'Clear' : "Clear"}</button>
           </div>
-          <label htmlFor="random">Randomize the grid</label>
-          <div name='random' className="random-containter button-container">
-            <button onClick={() => {
-              clear()
-              setGrid(random())
-            }}>Random</button>
-          </div>
-          <label htmlFor="meth">Seed the grid with one of my favorite methuselahs</label>
-          <div name='meth' className="methuselah-container button-container">
-            <button onClick={() => {
-              clear()
-              setGrid(acorn())
-            }}>Acorn</button>
-            <button onClick={() => {
-              clear()
-              setGrid(rPentomino())
-            }}>rPentomino</button>
-            <button onClick={() => {
-              clear()
-              setGrid(thunderbird())
-            }}>Thunderbird</button>
-          </div>
+
           <h3>Generation: {generation}</h3>
-          <h2 className="rules">About Game of Life:</h2>
-          <p className="about">
-            <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">The Game of Life</a> is a cellular automaton devised by the British mathematician 
-            John Horton Conway in 1970. It is a zero-player game, meaning that its evolution 
-            is determined by its initial state, requiring no further input.
-            One interacts with the Game of Life by creating an initial configuration and 
-            observing how it evolves.
-          </p>
-          <h2 className="rules">Rules:</h2>
-          <ol className="rules">
-            <li>Any live cell with two or three live neighbours survives.</li>
-            <li>Any dead cell with three live neighbours becomes a live cell.</li>
-            <li>All other live cells die in the next generation.</li> 
-            <li>Similarly, all other dead cells stay dead.</li>
-          </ol>
-          <h2 className="rules">This Implementation:</h2>
-          <p className="implementation">
-            This version of Conway's GOL was created by Ben Rogers.
-            He made it in React as a hook.
-            The Data-Structure is currently a 2-dimensional Array.
-            You can find the source <a href="https://github.com/thisbenrogers/gol-conway">here</a>
-          </p>
         </div>
         <div className="grid-container">
           <div className="grid">
